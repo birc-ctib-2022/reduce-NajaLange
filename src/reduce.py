5b45164def158ea75ddf680bba0c0986b3feb8fb
NajaLange <201906591@post.au.dk>
2022-10-07 10:32:45 +0200

    done

--- src/reduce.py
+++ src/reduce.py
@@ -13,7 +13,13 @@ def reduce(f: Callable[[A], B], x: list[A]) -> B:
     6
     """
     assert len(x) >= 2
-    ...  # FIXME
+    val = f(x[0], x[1])
+    for y in x[2:]:
+        val = f(val, y)
+    return val
+
+#print(reduce(lambda x,y: x+y, [1, 2, 3]))
+#print(reduce(lambda x,y: x*y, [1, 2, 4]))
 
 
 def accumulate(f: Callable[[A], A], x: list[A]) -> list[A]:
@@ -23,4 +29,13 @@ def accumulate(f: Callable[[A], A], x: list[A]) -> list[A]:
     >>> accumulate(lambda x,y: x+y, [1, 2, 3])
     [1, 3, 6]
     """
-    ...  # FIXME
+    assert len(x) >= 2
+    val = f(x[0], x[1])
+    lst = [x[0], val]
+    for y in x[2:]:
+        val = f(val, y)
+        lst.append(val)
+
+    return lst
+
+#print(accumulate(lambda x,y: x+y, [1, 2, 3]))
